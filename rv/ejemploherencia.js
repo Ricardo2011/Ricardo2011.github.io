b4caae553a8d76c2ab7f2eb1397d3e23f3da4854
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-04 14:10:10 -0600

    Update ejemploherencia.js

--- rv/ejemploherencia.js
+++ rv/ejemploherencia.js
@@ -1,7 +1,7 @@
 function Pierna(){
   THREE.Object3D.call(this);
   this.pierna = new THREE.Mesh( new THREE.BoxGeometry(1,5,1));
-  this.pierna = new THREE.Mesh( new THREE.BoxGeometry(2,1,1));
+  this.pie = new THREE.Mesh( new THREE.BoxGeometry(2,1,1));
   this.pierna.position.y=-2.5;
   this.pie.position.y=-4.5;
   this.pie.position.x=1;
