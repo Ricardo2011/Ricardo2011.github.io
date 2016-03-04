d44117fd2ed720ac2c448ff39f2bfd5e667fd147
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-04 14:01:18 -0600

    Create ejemploherencia.js

---
+++ rv/ejemploherencia.js
@@ -0,0 +1,43 @@
+function Pierna(){
+  THREE.Object3D.call(this);
+  this.pierna = new THREE.Mesh( new THREE.BoxGeometry(1,5,1));
+  this.pierna = new THREE.Mesh( new THREE.BoxGeometry(2,1,1));
+  this.pierna.position.y=-2.5;
+  this.pie.position.y=-4.5;
+  this.pie.position.x=1;
+  this.add(this.pierna);
+  this.add(this.pie);
+  }
+  Pierna.prototype= new THREE.Object3D();
+  funtion setup(){
+    var cuerpo= new THREE.Mesh( new THREE.CylinderGeometry(1,2,5,10));
+    piernaD = new Pierna();
+    piernaI = new Pierna();
+    cuerpo.position.y=2;
+    piernaD.position.z = -1;
+    piernaI.position.z = 1;
+    step = .01;
+    escena = new THREE.Scene();
+    escena.add(cuerpo);
+    escena.add(piernaD);
+    escena.add(piernaI);
+    camara =new THREE.WebGLRenderer();
+    renderer.setsize(window.innerHeight*.95, window.inner.Height*.95);
+    document.body.appendChild(renderer.domElement);
+  }
+  function loop(){
+    requestAnimationFrame(loop);
+    renderer.render(escena,camara);
+    if(Math.abs(piernaD.rotation.z)>.5)
+      step =-step;
+    
+    piernaD.rotation.z +=step;
+    piernaI.rotation.z -=step;
+  }
+  var escena,camara,renderer;
+  var step,piernaD,piernaI;
+  
+  setup();
+  loop();
+    
+    
