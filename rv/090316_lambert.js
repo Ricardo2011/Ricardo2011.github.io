e941eb147457336f387beab3e74e68af834ec4ae
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-09 11:15:19 -0600

    Update 090316_lambert.js

--- rv/090316_lambert.js
+++ rv/090316_lambert.js
@@ -1,9 +1,9 @@
 function setup(){
   var forma = new THREE.BoxGeometry(1, 1, 1 );
-  var material = new THREE.MeshLambertMaterial({color: '#ffffff'});
+  var material = new THREE.MeshLambertMaterial({{color: 0xffffff}});
   malla = new THREE.Mesh(forma, material);
   
-  var luzPuntual = new THREE.PointLight(0xcc00cc);
+  var luzPuntual = new THREE.PointLight(0xCC00CC);
   luzPuntual.position.x = 10; 
   luzPuntual.position.y = 10; 
   luzPuntual.position.z = 10; 
