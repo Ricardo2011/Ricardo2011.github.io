a74f5346331881f0744a012421983ee71980aea2
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:10:52 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -9,7 +9,9 @@ escena=new THREE.Scene();
 escena.add(malla);
 
 camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=20;
+camara.position.x=2;
+camara.position.y=5;
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
