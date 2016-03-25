0dd9c9105e853ef72e6012eaea999c61290c57c2
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:44:18 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -7,9 +7,9 @@ THREE.ImageUtils.crossOrigin='';
  var abajo = new THREE.Mesh(forma1, material1);*/
 
  //var textura2 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
-var geometry = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var cylinder = new THREE.Mesh( geometry, material );
+var forma = new THREE.CylinderGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var llanta1 = new THREE.Mesh( forma, material );
 //POSICIONES DE LOS ELEMENTOS
 //abajo.position.z=-1;
 //llanta1.rotation.z=1.57;
@@ -17,7 +17,7 @@ var cylinder = new THREE.Mesh( geometry, material );
 //llanta1.position.x=-3.5
 
 escena=new THREE.Scene();
-escena.add(cylinder);
+escena.add(llanta1);
 
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
