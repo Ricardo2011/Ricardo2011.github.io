ad103b0f5f9087ee2ba17425c421c9d94a27f866
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:13:32 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -3,10 +3,10 @@ THREE.ImageUtils.crossOrigin='';
  var textura1 = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
  var material1 = new THREE.MeshBasicMaterial({map:textura1});
  var forma1 = new THREE.BoxGeometry(6,6,1);
- malla = new THREE.Mesh(forma1, material1);
+ var abajo = new THREE.Mesh(forma1, material1);
 
 escena=new THREE.Scene();
-escena.add(malla);
+escena.add(abajo);
 
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
