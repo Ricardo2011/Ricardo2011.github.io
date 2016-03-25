4609093b29702023666d0e899831f626e087b102
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:49:46 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -8,13 +8,13 @@ THREE.ImageUtils.crossOrigin='';
 
  //var textura2 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
 var forma = new THREE.CylinderGeometry( 2, 2, 1, 10 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+var material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
 var llanta1 = new THREE.Mesh( forma, material );
 //POSICIONES DE LOS ELEMENTOS
 //abajo.position.z=-1;
 llanta1.rotation.z=1.57;
 llanta1.position.z=-1;
-//llanta1.position.x=-3.5
+llanta1.position.x=-3.5
 
 escena=new THREE.Scene();
 escena.add(llanta1);
