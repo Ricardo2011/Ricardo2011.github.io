587a9ec2207cd5d01c7aef220ff2d9d94159b649
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:23:58 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -1,18 +1,24 @@
 function setup(){
 THREE.ImageUtils.crossOrigin='';
 //TEXTURAS 
- var textura1 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
+/* var textura1 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
  var material1 = new THREE.MeshBasicMaterial({map:textura1});
  var forma1 = new THREE.BoxGeometry(6,6,1);
- var abajo = new THREE.Mesh(forma1, material1);
+ var abajo = new THREE.Mesh(forma1, material1);*/
 
+ var textura2 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
+ var material2 = new THREE.MeshBasicMaterial({map:textura2});
+ var forma2 = new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10));
+ var llanta1 = new THREE.Mesh(forma1, material1);
 
 //POSICIONES DE LOS ELEMENTOS
-abajo.position.z=-1;
-
+//abajo.position.z=-1;
+llanta1.rotation.z=1.57;
+llanta1.position.z=-1;
+llanta1.position.x=-3.5
 
 escena=new THREE.Scene();
-escena.add(abajo);
+escena.add(llanta1);
 
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
