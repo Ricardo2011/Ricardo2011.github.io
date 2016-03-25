9bc6dbf8d203da8146d167f6a25565ee2ba7a18d
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-25 16:18:24 -0600

    Update movil_textura.js

--- rv/movil_textura.js
+++ rv/movil_textura.js
@@ -1,6 +1,7 @@
 function setup(){
 THREE.ImageUtils.crossOrigin='';
- var textura1 = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/crate.gif');
+ 
+ var textura1 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
  var material1 = new THREE.MeshBasicMaterial({map:textura1});
  var forma1 = new THREE.BoxGeometry(6,6,1);
  var abajo = new THREE.Mesh(forma1, material1);
