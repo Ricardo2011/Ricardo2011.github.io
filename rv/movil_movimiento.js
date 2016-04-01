a0c7f77b61baa4f97aee77a2328f243c727c4b2c
Ricardo2011 <extranjero_92@hotmail.com>
2016-03-31 22:23:38 -0600

    Update movil_movimiento.js

--- rv/movil_movimiento.js
+++ rv/movil_movimiento.js
@@ -48,40 +48,16 @@ Abajo.prototype=new THREE.Object3D();
 
 function setup(){
 THREE.ImageUtils.crossOrigin='';
-//TEXTURAS 
- /*var textura1 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
- var material1 = new THREE.MeshBasicMaterial({map:textura1});
- var forma1 = new THREE.BoxGeometry(6,6,1);
- var abajo = new THREE.Mesh(forma1, material1);
-
- var textura = THREE.ImageUtils.loadTexture('http://thumbs.dreamstime.com/t/sandstone-texture-10289057.jpg');
- var forma = new THREE.CylinderGeometry( 2, 2, 1, 10 );
- var material = new THREE.MeshBasicMaterial( {map: textura} );
- var llanta1 = new THREE.Mesh( forma, material );
- 
- var textura2 = THREE.ImageUtils.loadTexture('http://thumbs.dreamstime.com/t/sandstone-texture-10289057.jpg');
- var forma2 = new THREE.CylinderGeometry( 2, 2, 1, 10 );
- var material2 = new THREE.MeshBasicMaterial( {map: textura2} );
- var llanta2 = new THREE.Mesh( forma2, material2 );*/
- 
 //POSICIONES DE LOS ELEMENTOS
 arriba = new Tapa();
 abajo1 = new Abajo();
-/*//abajo.position.z=-1;
-llanta1.rotation.z=1.57;
-llanta1.position.z=-1;
-llanta1.position.x=-3.5
-llanta2.rotation.z=1.57;
-llanta2.position.z=-1;
-llanta2.position.x=3.5;
-*/
+
 step=.01;
 
 escena=new THREE.Scene();
 escena.add(arriba);
 escena.add(abajo1);
-/*escena.add(llanta1);
-escena.add(llanta2);*/
+
 camara=new THREE.PerspectiveCamera();
 camara.position.z=20;
 camara.position.x=2;
@@ -98,12 +74,20 @@ controls.lookSpeed=0.05;
 controls.noFly=true;
 controls.lookVertical=false;
 
+clock1=new THREE.Clock();
+controls1=new THREE.FirstPersonControls(arriba);
+controls1.movementSpeed=10;
+controls1.lookSpeed=0.05;
+controls1.noFly=true;
+controls1.lookVertical=false;
+
 }
 
 function loop(){
 requestAnimationFrame(loop);
 renderer.render(escena,camara);
 controls.update(clock.getDelta());
+controls1.update(clock1.getDelta());
 
 if(Math.abs(arriba.rotation.z)>.4)
 step=-step;
@@ -113,7 +97,7 @@ arriba.rotation.z+=step;
 
 var escena,camara,renderer;
 var step,arriba;
-var clock,controls;
+var clock,controls,clock1,controls1;
 
 setup();
 loop();
