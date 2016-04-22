f37ab9fe1b930ad6e6c337a72ec2f716d0e40607
Ricardo2011 <extranjero_92@hotmail.com>
2016-04-21 22:49:34 -0500

    Update movil_sombra.js

--- rv/movil_sombra.js
+++ rv/movil_sombra.js
@@ -1,9 +1,13 @@
 function Tapa(){
 
 THREE.Object3D.call(this);
+THREE.ImageUtils.crossOrigin='';
 
-this.canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1),new THREE.MeshPhongMaterial({color:'#00cc00'}));
-this.sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1),new THREE.MeshPhongMaterial({color:'#00cc00'}));
+var textura3 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
+//var textura4 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
+
+this.canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1), new THREE.MeshBasicMaterial({map: textura3}));
+this.sujetador=new THREE.Mesh(new THREE.BoxGeometry(1,6,1), new THREE.MeshBasicMaterial({map: textura3}));
 
 this.canon.position.y=0;
 this.sujetador.position.y=3;
@@ -15,9 +19,20 @@ this.add(this.sujetador);
 Tapa.prototype=new THREE.Object3D();
 
 function setup(){
-var abajo=new THREE.Mesh(new THREE.BoxGeometry(6,6,1),new THREE.MeshPhongMaterial({color:'#00cc00'}));
-var llanta1=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10),new THREE.MeshPhongMaterial({color:'#00cc00'}));
-var llanta2=new THREE.Mesh(new THREE.CylinderGeometry(2,2,1,10),new THREE.MeshPhongMaterial({color:'#00cc00'}));
+ var textura1 = THREE.ImageUtils.loadTexture('http://i39.servimg.com/u/f39/16/68/03/45/810.jpg');
+ var material1 = new THREE.MeshBasicMaterial({map:textura1});
+ var forma1 = new THREE.BoxGeometry(6,6,1);
+ var abajo = new THREE.Mesh(forma1, material1);
+
+ var textura = THREE.ImageUtils.loadTexture('http://thumbs.dreamstime.com/t/sandstone-texture-10289057.jpg');
+ var forma = new THREE.CylinderGeometry( 2, 2, 1, 10 );
+ var material = new THREE.MeshBasicMaterial( {map: textura} );
+ var llanta1 = new THREE.Mesh( forma, material );
+ 
+ var textura2 = THREE.ImageUtils.loadTexture('http://thumbs.dreamstime.com/t/sandstone-texture-10289057.jpg');
+ var forma2 = new THREE.CylinderGeometry( 2, 2, 1, 10 );
+ var material2 = new THREE.MeshBasicMaterial( {map: textura2} );
+ var llanta2 = new THREE.Mesh( forma2, material2 );
 
 var base = new THREE.Mesh(new THREE.BoxGeometry(15,10,.1), new THREE.MeshLambertMaterial({color: 0xF0D58C}));
 
