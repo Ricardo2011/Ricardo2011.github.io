c646b6daec8924341ee775b34955d597f61e3598
Ricardo2011 <extranjero_92@hotmail.com>
2016-04-24 10:39:53 -0500

    Update movil_luz.js

--- rv/movil_luz.js
+++ rv/movil_luz.js
@@ -65,8 +65,9 @@ escena.add(cubo2);
 escena.add(cubo3);
 escena.add(cubo4);
 escena.add(pelota);
+escena.add(luzconica);
 escena.add(camara);
-escena.add(luzconica)
+
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
