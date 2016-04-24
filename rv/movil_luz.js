b9796088c59cc494e9867e7ec47d967cf793d575
Ricardo2011 <extranjero_92@hotmail.com>
2016-04-24 09:21:40 -0500

    Update movil_luz.js

--- rv/movil_luz.js
+++ rv/movil_luz.js
@@ -1,8 +1,8 @@
 function setup(){
 cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,40,5),new THREE.MeshLambertMaterial({color:'#ffff00'}));
-cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,40,5),new THREE.MeshBasicMaterial({color:'#ffff00'}));
+cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,40,5),new THREE.MeshLambertMaterial({color:'#ffff00'}));
 
-cubo3=new THREE.Mesh(new THREE.BoxGeometry(40,1,5),new  THREE.MeshLambertMaterial({color:'#ffff00'}));
+cubo3=new THREE.Mesh(new THREE.BoxGeometry(40,1,5),new  THREE.MeshBasicMaterial({color:'#ffff00'}));
 cubo4=new THREE.Mesh(new THREE.BoxGeometry(40,1,5),new  THREE.MeshLambertMaterial({color:'#ffff00'}));
 
 var canon=new THREE.Mesh(new THREE.BoxGeometry(4,4,1));
