8e656482b044fe79a785ced814e295a2deee1513
Ricardo2011 <extranjero_92@hotmail.com>
2016-04-24 12:17:22 -0500

    Update movil_luz.js

--- rv/movil_luz.js
+++ rv/movil_luz.js
@@ -51,7 +51,7 @@ raycaster4=new THREE.Raycaster(pelota.position,new THREE.Vector3(0,-1,0));
 var luzconica = new THREE.SpotLight( 0xffffff );
 pelota.add(luzconica);
 luzconica.position.set(-5,-5,0);
-luzconica.target = pelota;
+//luzconica.target = pelota;
 //luzconica.position.set( -5,-5,0);
 luzconica.intensity = 1;
 //------------
