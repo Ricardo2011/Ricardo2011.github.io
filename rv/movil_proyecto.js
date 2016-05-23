290130c7c6b1ae7b1ed8bda1edb613bd66696d52
Ricardo2011 <extranjero_92@hotmail.com>
2016-05-22 20:09:40 -0500

    Update movil_proyecto.js

--- rv/movil_proyecto.js
+++ rv/movil_proyecto.js
@@ -2,7 +2,7 @@ function setup(){
 cubo1=new THREE.Mesh(new THREE.BoxGeometry(0.5,60,3),new THREE.MeshBasicMaterial({color:'#ffff00'}));
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(0.5,60,3),new THREE.MeshBasicMaterial({color:'#ffff00'}));
 
-cubo1.position.x = 10;
+cubo1.position.x = 100;
 
 cubo3=new THREE.Mesh(new THREE.BoxGeometry(60,0.5,3),new  THREE.MeshBasicMaterial({color:'#ffff00'}));
 cubo4=new THREE.Mesh(new THREE.BoxGeometry(60,0.5,3),new  THREE.MeshBasicMaterial({color:'#ffff00'}));
